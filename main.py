92e465dc8476ecce09baa1ee9cde7dfc6c8bf505
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-04-04 20:15:06 -0300

    login

--- main.py
+++ main.py
@@ -2,21 +2,56 @@ from flask import Flask, render_template, request, redirect, url_for
 from db import db
 from models import Usuarios
 # from views import *
+from flask_login import LoginManager, login_user, login_required, logout_user
 
 app = Flask(__name__)
+app.secret_key = 'schorrocode'
+lm = LoginManager(app)
+lm.login_view = 'registrar'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bank.db'
 db.init_app(app)
 
+
+@lm.user_loader
+def load_user(id):
+    usuario = db.session.query(Usuarios).filter_by(id=id).first()
+    return usuario
+
+
 @app.route('/')
+@login_required
 def home():
     return render_template('home.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    elif request.method == 'POST':
+        nome = request.form.get('nome')
+        senha = request.form.get('senha')
+
+        user = db.session.query(Usuarios).filter_by(nome=nome, senha=senha).first()
+        if not user:
+            return 'Usuário ou senha incorretos'
+        login_user(user)
+        return redirect(url_for('home'))
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 @app.route('/registrar', methods=['GET', 'POST'])
 def registrar():
     if request.method == 'GET':
         return render_template('register.html')
-    
+
     elif request.method == 'POST':
         nome = request.form.get('nome')
         senha = request.form.get('senha')
@@ -25,7 +60,10 @@ def registrar():
         db.session.add(novo_usuario)
         db.session.commit()
 
+        login_user(novo_usuario) 
+
         return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     with app.app_context():
--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 # Aqui é para banco de dados
 from db import db
+from flask_login import UserMixin
 
 
-class Usuarios(db.Model):
+class Usuarios(UserMixin, db.Model):
     __tablename__ = 'usuarios'
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(30), unique=True)
-    senha = db.Column(db.String())
+    senha = db.Column(db.String(100))
